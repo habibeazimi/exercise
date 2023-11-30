6b5f2682dd7d77e056fd2906886d824b4455e901
habibeazimi <152497190+habibeazimi@users.noreply.github.com>
2023-11-30 15:24:24 +0330

    Add files via upload

---
+++ exercise2.2.py
@@ -0,0 +1,11 @@
+h = input("enter ca(+, -,*,/):")
+a = int(input("enter a: "))
+b = int(input("enter b:"))
+if h == "+":
+    print(a+b)
+if h =="-":
+    print(a-b)
+if h =="*":
+    print(a*b)    
+if h =="/":
+    print(a/b)
---
+++ exercise2.3.py
@@ -0,0 +1,7 @@
+a=int (input("enter a:"))
+b=int (input("enter b:"))
+c=int (input("enter c:"))
+if a+b>c and a+c>b and c+b>a:
+    print("it is possible")
+else:
+    print("it is impossible")    
